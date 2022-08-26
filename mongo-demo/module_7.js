5fb2670223f22575a4862fbb95c10a184a8fdb87
Anurag_Yadav <ay1909069@gmail.com>
2022-08-26 13:44:08 +0530

    m1

--- mongo-demo/module_7.js
+++ mongo-demo/module_7.js
@@ -80,9 +80,58 @@
 
 
 
+// //-----------------------------------------------------------------
+
+// // Lec 7 Saving a Document
+
+// // loading mongoose object
+// const { connect } = require('mongoose')
+// const mongoose = require('mongoose')
+
+// //connecting to the mongoDB database
+
+// // playground-> name of the database
+// // connect method returns promise
+// mongoose.connect('mongodb://localhost:27017/playground') // takes connection string
+//     .then(()=> console.log('connected to MongoDB......'))
+//     .catch(err => console.error('Could not connect to MongoDB'))
+
+// // defining the schema
+// const courseSchema = new mongoose.Schema({
+//     name: String,
+//     author: String,
+//     tags: [String],
+//     date: { type: Date, default: Date.now },
+//     isPulished: Boolean
+// })
+
+// // Making the model
+
+// // returns class
+// const Course = mongoose.model('Course', courseSchema)
+
+// async function createCourse() {
+//     const course = new Course({
+//         name: 'Angular Course',
+//         author: 'Mosh',
+//         tags: ['angular', 'frontend'],
+//         isPublished: true
+//     })
+
+//     // saving the document in the database****
+//     // it is asynchronous operation
+//     // this method returns a promise
+//     // when we save this course in mongoDb it wil assing this a id
+//     const result = await course.save()
+//     console.log(result)
+// }
+// createCourse()
+
+
+
 //-----------------------------------------------------------------
 
-// Lec 7 Saving a Document
+// Lec 8 Querying a document
 
 // loading mongoose object
 const { connect } = require('mongoose')
@@ -112,9 +161,9 @@ const Course = mongoose.model('Course', courseSchema)
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node.js Course',
+        name: 'Angular Course',
         author: 'Mosh',
-        tags: ['node', 'backend'],
+        tags: ['angular', 'frontend'],
         isPublished: true
     })
 
@@ -125,4 +174,22 @@ async function createCourse() {
     const result = await course.save()
     console.log(result) 
 }
-createCourse()
+
+
+// querying a document***
+
+async function getCourses() {
+    // finding all document
+    //const courses = await Course.find()
+
+    // more complex query
+    const courses = await Course
+        .find({ author: 'Mosh'})
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 })
+    
+    
+    console.log(courses)
+}
+getCourses()
