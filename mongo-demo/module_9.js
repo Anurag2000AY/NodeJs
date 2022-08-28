b1591fac911cb879259406148717f9fb6ba96509
Anurag_Yadav <ay1909069@gmail.com>
2022-08-28 16:32:28 +0530

    done Module9

--- mongo-demo/module_9.js
+++ mongo-demo/module_9.js
@@ -163,85 +163,221 @@
 // listCourses()
 
 
-//=============================================================================
-// Lec 4 Embedding Documents
+// //=============================================================================
+// // Lec 4 Embedding Documents
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB...', err))
-
-
-const authorSchema = new mongoose.Schema({ 
-    name: String,
-    bio: String,
-    website: String
-})
-
-const Author = mongoose.model('Author', authorSchema)
-
-const Course = mongoose.model('Course', new mongoose.Schema({
-    name: String,
-    author: authorSchema
-}))
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/playground')
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
 
 
-async function createAuthor(name, bio, website) {
-    const author = new Author({
-        name,
-        bio,
-        website
-    })
-    const result = await author.save()
-    console.log(result)
-}
+// const authorSchema = new mongoose.Schema({
+//     name: String,
+//     bio: String,
+//     website: String
+// })
 
-async function createCourse(name, author) {
-    const course = new Course({
-        name,
-        author
-    })
-    const result = await course.save()
-    console.log(result)
-}
+// const Author = mongoose.model('Author', authorSchema)
 
-async function listCourses() {
-    const courses = await Course.find()
-    console.log(courses)
-}
-
-// Updating the name of author in document
-async function updateAuthor(courseId) {
-    //const course = await Course.findById(courseId)
-
-    // udating the sub document directly in database
-    // const cosurse = await Course.update({ _id: courseId }, {
-    //     // updating
-    //     $set: {
-    //         // write name of the properties you want to update
-    //         'author.name': 'Jhon'
-    //     }
-    // })
+// const Course = mongoose.model('Course', new mongoose.Schema({
+//     name: String,
+//     author: authorSchema
+// }))
 
 
-    // removing the subdocument
-    const course = await Course.update({ _id: courseId }, {
-        // updating
-        $unset: {
-            // write name of the properties you want to update
-            'author': ''
-        }
-    })
-    // course.author.name = 'Anurag'
-    // course.save()
-    console.log(course)
+// async function createAuthor(name, bio, website) {
+//     const author = new Author({
+//         name,
+//         bio,
+//         website
+//     })
+//     const result = await author.save()
+//     console.log(result)
+// }
 
-    // note course.Author.save dosen't exist
+// async function createCourse(name, author) {
+//     const course = new Course({
+//         name,
+//         author
+//     })
+//     const result = await course.save()
+//     console.log(result)
+// }
 
-}
+// async function listCourses() {
+//     const courses = await Course.find()
+//     console.log(courses)
+// }
 
-//createAuthor('Anurag', 'My bio', 'My website')
-// createCourse('Node Course', new Author({ name: 'Mosh'}))
-// listCourses()
+// // Updating the name of author in document
+// async function updateAuthor(courseId) {
+//     //const course = await Course.findById(courseId)
 
-updateAuthor('630a02f2bac04584e8ea6e5c')
+//     // udating the sub document directly in database
+//     // const cosurse = await Course.update({ _id: courseId }, {
+//     //     // updating
+//     //     $set: {
+//     //         // write name of the properties you want to update
+//     //         'author.name': 'Jhon'
+//     //     }
+//     // })
+
+
+//     // removing the subdocument
+//     const course = await Course.update({ _id: courseId }, {
+//         // updating
+//         $unset: {
+//             // write name of the properties you want to update
+//             'author': ''
+//         }
+//     })
+//     // course.author.name = 'Anurag'
+//     // course.save()
+//     console.log(course)
+
+//     // note course.Author.save dosen't exist
+
+// }
+
+// //createAuthor('Anurag', 'My bio', 'My website')
+// // createCourse('Node Course', new Author({ name: 'Mosh'}))
+// // listCourses()
+
+// updateAuthor('630a02f2bac04584e8ea6e5c')
+
+
+
+// //=============================================================================
+// // Lec 5 Using an array of sub-documents
+
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/playground')
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err))
+
+
+// const authorSchema = new mongoose.Schema({
+//     name: String,
+//     bio: String,
+//     website: String
+// })
+
+// const Author = mongoose.model('Author', authorSchema)
+
+// const Course = mongoose.model('Course', new mongoose.Schema({
+//     name: String,
+//     // author: authorSchema
+
+//     //using array of subdocuments
+
+//     authors: [authorSchema]
+
+// }))
+
+
+// async function createAuthor(name, bio, website) {
+//     const author = new Author({
+//         name,
+//         bio,
+//         website
+//     })
+//     const result = await author.save()
+//     console.log(result)
+// }
+
+// async function createCourse(name, authors) {
+//     const course = new Course({
+//         name,
+//         authors
+//     })
+//     const result = await course.save()
+//     console.log(result)
+// }
+
+// async function listCourses() {
+//     const courses = await Course.find()
+//     console.log(courses)
+// }
+
+// // Updating the name of author in document
+// async function updateAuthor(courseId) {
+//     //const course = await Course.findById(courseId)
+
+//     // udating the sub document directly in database
+//     // const cosurse = await Course.update({ _id: courseId }, {
+//     //     // updating
+//     //     $set: {
+//     //         // write name of the properties you want to update
+//     //         'author.name': 'Jhon'
+//     //     }
+//     // })
+
+
+//     // removing the subdocument
+//     const course = await Course.update({ _id: courseId }, {
+//         // updating
+//         $unset: {
+//             // write name of the properties you want to update
+//             'author': ''
+//         }
+//     })
+//     // course.author.name = 'Anurag'
+//     // course.save()
+//     console.log(course)
+
+//     // note course.Author.save dosen't exist
+
+// }
+// // // adding data to array of subdocumetns***
+// // async function addAuthor(courseId, author) {
+// //     const course = await Course.findById(courseId)
+// //     course.authors.push(author)
+// //     course.save()
+// //     console.log(course)
+
+// // }
+
+// // removing data from the array of subdocuments
+// async function removeAuthor(courseId, authorId) {
+//     const course = await Course.findById(courseId)
+//     const author = course.authors.id(authorId)
+//     author.remove()
+//     course.save()
+//     console.log(author)
+// }
+// removeAuthor('630b321a9daf9c92b0155a4e', '630b321a9daf9c92b0155a4c')
+
+
+// //createAuthor('Anurag', 'My bio', 'My website')
+
+
+// // creating array of authors
+// // createCourse('Node Course', [
+// //     new Author({name: 'Mosh'}),
+// //     new Author({name: 'Jhon'})
+// // ])
+
+// // adding author to the subdocument
+// // addAuthor('630b321a9daf9c92b0155a4e', new Author({name: 'Anurag'}))
+
+// // listCourses()
+
+// //updateAuthor('630a02f2bac04584e8ea6e5c')
+
+
+// // Lec 9 ObjectID
+
+// const mongoose = require('mongoose')
+
+// // generating the new ObjectId
+// const id = new mongoose.Types.ObjectId()
+// console.log(id)
+
+
+// // extracting timestamp from ObjectID
+// console.log(id.getTimestamp())
+
+// // validating ObjectId
+// const isValid = mongoose.Types.ObjectId.isValid('324')
+// console.log(isValid)
